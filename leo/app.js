80f77a628313e67517efaf80f6bad58862876ec6
siegfriedjahnel <34278169+siegfriedjahnel@users.noreply.github.com>
2018-10-03 11:53:29 +0200

    Update app.js

--- leo/app.js
+++ leo/app.js
@@ -26,7 +26,7 @@ function dayAsString(unixDay){
 	return dayString;
 }
 //------
-fetch(apiUrl, {mode:no-cors''})
+fetch(apiUrl, {mode:'no-cors'})
 .then(function(response) {
 	return response.json();
 })
