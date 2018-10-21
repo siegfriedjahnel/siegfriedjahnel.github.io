7d6a7ea929440aef40e6e76c611afa556c21f172
siegfriedjahnel <info@sj-sam.de>
2018-10-21 19:06:29 +0200

    fb3-v1

---
+++ fb3/teamNamesCl.js
@@ -0,0 +1,38 @@
+	
+
+var teamNamesCl = {"cID":"5","Name":"Champions League","Letter":"CL",
+"teams":{
+	"t1":{"Name":"Manchester United",	"Short":"Man United","Letter":"MAN"},
+	"t121":{"Name":"AS Rom","Short":"AS Rom","Letter":"ROM"},
+	"t127":{"Name":"Inter Mailand","Short":"Inter","Letter":"INT"},
+	"t128":{"Name":"Juventus Turin","Short":"Juventus","Letter":"JUV"},
+	"t1340":{"Name":"ZSKA Moskau","Short":"ZSKA","Letter":"ZSK"},
+	"t14":{"Name":"Liverpool FC","Short":"Liverpool","Letter":"LIV"},
+	"t143":{"Name":"Olympique Lyon","Short":"Lyon","Letter":"LYO"},
+	"t146":{"Name":"AS Monaco","Short":"Monaco","Letter":"MON"},
+	"t149":{"Name":"Paris St. Germain","Short":"Paris","Letter":"PSG"},
+	"t156":{"Name":"FC Bayern München","Short":"FC Bayern","Letter":"FCB"},
+	"t157":{"Name":"Borussia Dortmund","Short":"Dortmund","Letter":"BVB"},
+	"t167":{"Name":"FC Schalke 04","Short":"Schalke","Letter":"S04"},
+	"t175":{"Name":"Atlético Madrid","Short":"Atlético","Letter":"ATL"},
+	"t178":{"Name":"FC Barcelona","Short":"Barcelona","Letter":"BAR"},
+	"t186":{"Name":"Real Madrid","Short":"Real","Letter":"REA"},
+	"t1902":{"Name":"1899 Hoffenheim","Short":"TSG 1899","Letter":"TSG"},
+	"t191":{"Name":"FC Valencia","Short":"Valencia","Letter":"VAL"},
+	"t1963":{"Name":"BSC Young Boys Bern","Short":"YB Bern","Letter":"YBB"},
+	"t201":{"Name":"FC Porto","Short":"Porto","Letter":"POR"},
+	"t204":{"Name":"PSV Eindhoven","Short":"Eindhoven","Letter":"PSV"},
+	"t208":{"Name":"Galatasaray Istanbul","Short":"Galatasaray","Letter":"GAL"},
+	"t214":{"Name":"Roter Stern Belgrad","Short":"Belgrad","Letter":"RSB"},
+	"t215":{"Name":"Ajax Amsterdam","Short":"Ajax","Letter":"AMS"},
+	"t234":{"Name":"AEK Athen","Short":"Athen","Letter":"AEK"},
+	"t251":{"Name":"Benfica Lissabon","Short":"Benfica","Letter":"BEN"},
+	"t2703":{"Name":"Viktoria Pilsen","Short":"Pilsen","Letter":"PIL"},
+	"t271":{"Name":"Lokomotive Moskau","Short":"Moskau","Letter":"LOK"},
+	"t413":{"Name":"FC Brügge","Short":"Brügge","Letter":"BRÜ"},
+	"t43":{"Name":"Manchester City","Short":"Man City","Letter":"MAC"},
+	"t455":{"Name":"Schachtior Donezk","Short":"Donezk","Letter":"DON"},
+	"t459":{"Name":"SSC Neapel","Short":"Neapel","Letter":"SSC"},
+	"t6":{"Name":"Tottenham Hotspur","Short":"Tottenham","Letter":"TOT"}
+}
+}
