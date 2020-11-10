b8cda6f7fd1b277aa9b0e7bc90a59fee872a7167
siegfriedjahnel <info@sj-sam.de>
2020-11-10 23:36:07 +0100

    first

---
+++ ichlernerechnen/app.js
@@ -0,0 +1,81 @@
+let tasten = document.getElementsByClassName("taste");
+let operatoren = document.getElementsByClassName("operator");
+
+let ergebnisFeld = document.getElementById("ergebnisFeld");
+let operatorFeld = document.getElementById("operatorFeld");
+let term1Feld = document.getElementById("term1Feld");
+let term2Feld = document.getElementById("term2Feld");
+
+let term1;
+let term2;
+let operator;
+let ergebnis;
+let max = 88;
+Array.prototype.filter.call(tasten, function(element){
+    //console.log(element);
+    element.addEventListener("click", function(){
+        if(this.value =="clear"){
+            ergebnisFeld.innerHTML="";
+        }else if(this.value=="enter"){
+                check(ergebnisFeld.innerHTML)
+        }else{
+            ergebnisFeld.innerHTML += this.value;
+
+        }
+
+    })
+
+});
+
+Array.prototype.filter.call(operatoren, function(element){
+    element.addEventListener("click", function(){
+        ergebnisFeld.innerHTML="";
+        if(this.id=="+"){
+            operatorFeld.innerHTML="+";
+            console.log("plusaufgabe");
+            term1 = getRandomInt(max);
+            term2 = getRandomInt(max);
+            ergebnis = term1+term2;
+            term1Feld.innerHTML=term1;
+            term2Feld.innerHTML=term2;
+        }
+        if(this.id=="-"){
+            operatorFeld.innerHTML="-";
+            console.log("minusaufgabe");
+            term1 = getRandomInt(max);
+            term2 = getRandomInt(max);
+            if(term1>term2){
+                ergebnis = term1-term2;
+                term1Feld.innerHTML=term1;
+                term2Feld.innerHTML=term2;
+            }else{
+                ergebnis = term2-term1;
+                term1Feld.innerHTML=term2;
+                term2Feld.innerHTML=term1;
+            }
+        }
+
+    });
+});
+
+
+
+function check(vorschlag){
+    if(ergebnis==vorschlag){
+        console.log("Richtig");
+        PlaySound("good");
+    }else{
+        PlaySound("bad");
+        ergebnisFeld.innerHTML="";
+    }
+
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function PlaySound(soundObj) {
+    var sound = document.getElementById(soundObj);
+    console.log(sound);
+    sound.Play();
+  }
