f926bec721475636e628ca906c3493c3ff485e3a
siegfriedjahnel <info@sj-sam.de>
2020-11-15 11:49:12 +0100

    pwa-style

--- ichlernerechnen/app.js
+++ ichlernerechnen/app.js
@@ -1,5 +1,11 @@
+navigator.serviceWorker.register('sw.js');
+
 let tasten = document.getElementsByClassName("taste");
 let operatoren = document.getElementsByClassName("operator");
+
+let btn10 = document.getElementById("btn10");
+let btn20 = document.getElementById("btn20");
+let btn50 = document.getElementById("btn50");
 
 let ergebnisFeld = document.getElementById("ergebnisFeld");
 let operatorFeld = document.getElementById("operatorFeld");
@@ -11,9 +17,34 @@ let term1;
 let term2;
 let operator;
 let ergebnis;
-let max = 20;
+let max = 10; //die maximalen Zahlen
+btn10.style.backgroundColor = "lime";
+
+//-----------------------------------------
+btn10.addEventListener("click", function(){
+    max = this.value;
+    btn10.style.backgroundColor = "lime";
+    btn20.style.backgroundColor = "white";
+    btn50.style.backgroundColor = "white";
+})
+
+btn20.addEventListener("click", function(){
+    max = this.value;
+    btn20.style.backgroundColor = "lime";
+    btn10.style.backgroundColor = "white";
+    btn50.style.backgroundColor = "white";
+})
+
+btn50.addEventListener("click", function(){
+    max = this.value;
+    btn50.style.backgroundColor = "lime";
+    btn20.style.backgroundColor = "white";
+    btn10.style.backgroundColor = "white";
+})
+
+//-----------------------
+//------------------ Tastatur------------------------
 Array.prototype.filter.call(tasten, function(element){
-    //console.log(element);
     element.addEventListener("click", function(){
         if(this.value =="clear"){
             ergebnisFeld.innerHTML="";
@@ -21,20 +52,19 @@ Array.prototype.filter.call(tasten, function(element){
                 check(ergebnisFeld.innerHTML)
         }else{
             ergebnisFeld.innerHTML += this.value;
-
         }
-
     })
-
 });
+//------------------------------------------------------
 
+//--------------- Steuerung-------------------------------
 Array.prototype.filter.call(operatoren, function(element){
     element.addEventListener("click", function(){
         emojiFeld.innerHTML=`<img src="images/question-trans.png">`;
-
         ergebnisFeld.innerHTML="";
         istgleichFeld.innerHTML="=";
-        if(this.id=="+"){
+
+        if(this.value == "+"){
             operatorFeld.innerHTML="+";
             ergebnisFeld.innerHTML="";
             console.log("plusaufgabe");
@@ -44,7 +74,7 @@ Array.prototype.filter.call(operatoren, function(element){
             term1Feld.innerHTML=term1;
             term2Feld.innerHTML=term2;
         }
-        if(this.id=="-"){
+        if(this.value == "-"){
             operatorFeld.innerHTML="-";
             console.log("minusaufgabe");
             term1 = getRandomInt(max);
@@ -59,10 +89,28 @@ Array.prototype.filter.call(operatoren, function(element){
                 term2Feld.innerHTML=term1;
             }
         }
+        if(this.value == "x"){
+            operatorFeld.innerHTML="x";
+            term1 = getRandomInt(max);
+            term2 = getRandomInt(max);
+            ergebnis = term1 * term2;
+            term1Feld.innerHTML=term1;
+            term2Feld.innerHTML=term2;
+        }
+        if(this.value == "/"){
+            operatorFeld.innerHTML="/";
+            term1 = getRandomInt(max);
+            term2 = getRandomInt(max);
+            let mult = term1*term2;
+            term1Feld.innerHTML = mult;
+            term2Feld.innerHTML = term1;
+            ergebnis = term2;
+            
 
+        }
     });
 });
-
+//------------------------------------------------------
 
 
 function check(vorschlag){
@@ -80,7 +128,7 @@ function check(vorschlag){
 
 }
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max))+1;
   }
 
   
