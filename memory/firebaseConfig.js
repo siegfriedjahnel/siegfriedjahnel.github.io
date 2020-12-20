fa8d558a6a0b0ae70681fd129b9a06bcfa49240a
siegfriedjahnel <info@sj-sam.de>
2020-12-20 21:10:35 +0100

    first-1

---
+++ memory/firebaseConfig.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDw_akOHokaCObViq03C5fI3QTPzQk9w_o",
+    authDomain: "app4-50a59.firebaseapp.com",
+    databaseURL: "https://app4-50a59.firebaseio.com",
+    projectId: "app4-50a59",
+    storageBucket: "app4-50a59.appspot.com",
+    messagingSenderId: "390654674553",
+    appId: "1:390654674553:web:092a5df880fa6f6516b6a8"
+  };
