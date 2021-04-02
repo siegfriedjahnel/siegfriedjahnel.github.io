99b8ce3e96a2c17edabf16f2a61741ff43517305
siegfriedjahnel <info@sj-sam.de>
2021-04-02 22:25:14 +0200

    css changed

--- covid/app.js
+++ covid/app.js
@@ -1,11 +1,17 @@
 
 
-//aoe= 09171 -- 226
-//mue= 09183 -- 238
-//const landkreis="09183";
-//const landkreis="09171";
 
-const landkreise = [
+
+const alle = [
+  { "name": "D", "AdminUnitId": "0", "BundeslandId": "0" },
+  { "name": "Bay", "AdminUnitId": "9", "BundeslandId": "9" },
+  { "name": "AÖ", "AdminUnitId": "9171", "BundeslandId": "9" },
+  { "name": "MÜ", "AdminUnitId": "9183", "BundeslandId": "9" },
+  { "name": "TS", "AdminUnitId": "9189", "BundeslandId": "9" },
+  { "name": "RO", "AdminUnitId": "9187", "BundeslandId": "9" },
+  { "name": "Rottal", "AdminUnitId": "9277", "BundeslandId": "9" },
+
+];const landkreise = [
   { "name": "AÖ", "landkreisId": "09171", "objektId": "226" },
   { "name": "MÜ", "landkreisId": "09183", "objektId": "238" },
   { "name": "TS", "landkreisId": "09189", "objektId": "244" },
@@ -13,14 +19,25 @@ const landkreise = [
   { "name": "Rottal", "landkreisId": "09277", "objektId": "255" },
 
 ];
+
+
+
 const lastUpdateSpan = document.getElementById("lastUpdate");
 const loader = document.getElementById("loader");
 var row = [];
 var table = [];
 var promises = [];
 
-// const uri_new_cases = `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=(NeuerFall%20IN(1%2C%20-1))%20AND%20(IdLandkreis%3D%27${landkreisId}%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlFall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true`;
-// const uri_new_death_cases =`https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=(NeuerTodesfall%20IN(1%2C%20-1))%20AND%20(IdLandkreis%3D%27${landkreisId}%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlTodesfall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true`;
+//https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?f=json
+//&where=(BundeslandId=${BundeslandId}) AND (AdmUnitId=${AdminUnitId})
+//&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=AdmUnitId asc&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true
+//
+//features[0].attributes.AnzFall
+//features[0].attributes.AnzFallNeu
+//features[0].attributes.AnzTodesfall
+//features[0].attributes.AnzTodesfallNeu
+//features[0].attributes.Inz7T
+
 
 landkreise.forEach(function (element, i) {
   var name = element.name;
@@ -32,6 +49,10 @@ landkreise.forEach(function (element, i) {
 
 })
 
+alle.forEach(function(element){
+  getAllData(element.name, element.BundeslandId,element.AdminUnitId);
+  // console.log("element: "+element.AdminUnitId);
+})
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 //lastUpdate.innerHTML="<img src='icons/loader.gif'></img>";
@@ -39,10 +60,25 @@ lastUpdateSpan.innerHTML = "loading Data....";
 
 Promise.all(promises)
 .then(function (result) {
-  console.log(result);
+  //console.log(result);
   loader.style.display='none';
   drawTable(result);
 })
+//---------------
+function getAllData(Name, BundeslandId, AdminUnitId){
+  let uri = `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?f=json&where=(BundeslandId=${BundeslandId})%20AND%20(AdmUnitId=${AdminUnitId})%20&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=AdmUnitId%20asc&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true`;
+  fetch(uri)
+  .then(response => response.json())
+  .then(data => {
+    let attr = data.features[0].attributes;
+    console.log(Name + "-" + attr.AnzFall + "-" + attr.AnzFallNeu+ "-" + attr.AnzTodesfall + "-" + attr.AnzTodesfallNeu+ "-" + attr.Inz7T);
+  })
+
+  //.then(data => console.log(Name +": " +data.features[0].attributes.AnzFall+"-"+data.features[0].attributes.AnzFallNeu));
+}
+
+
+
 //----------------------------
 getLastUpdate();
 function getLastUpdate() {
